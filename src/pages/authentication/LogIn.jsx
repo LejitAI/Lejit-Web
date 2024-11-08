271dfff074f4836fba070214224903c7ddbec256
C B Dev Narayan <devcb@pg.cusat.ac.in>
2024-11-08 07:30:09 +0530

    fix cors

--- src/pages/authentication/LogIn.jsx
+++ src/pages/authentication/LogIn.jsx
@@ -24,7 +24,10 @@ const LogIn = () => {
        const response = await fetch("https://lejit-backend-node.onrender.com/api/auth/login", {
           method: "POST",
           headers: { "Content-Type": "application/json" },
-          body: JSON.stringify({ email, password })
+          body: JSON.stringify({ 
+            "email": email, 
+            "password": password 
+          })
        });
  
        const data = await response.json();
