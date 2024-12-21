ea4316938731a4bb16b77c84845a741b75a1b4d0
Mariyaben <98140958+Mariyaben@users.noreply.github.com>
2024-12-21 22:21:34 +0530

    ip address updated

--- src/pages/authentication/LogIn.jsx
+++ src/pages/authentication/LogIn.jsx
@@ -21,7 +21,7 @@ const LogIn = () => {
     }
  
     try {
-       const response = await fetch("http://13.232.153.48:5000/api/auth/login", {
+       const response = await fetch("http://52.74.188.1:5000/api/auth/login", {
           method: "POST",
           headers: { "Content-Type": "application/json" },
           body: JSON.stringify({ 
